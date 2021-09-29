ed3be9825b846154b4e152804329d5ae544e9c38
Sajit Sasi <sajit.sasi@outlook.com>
2021-09-29 08:23:12 -0500

    updated stress source

--- src/stress/stress.sh
+++ src/stress/stress.sh
@@ -3,11 +3,13 @@
 
 # Step function in 5% increments for CPU utilization
 
+
 while [ 1 ]; do
   for i in `seq 5 5 100`; do
     echo "$(date -u) Starting CPU throttle for 5 minutes @ ${i}%"
     stress-ng --cpu -1 --cpu-method all -t ${timeout} --cpu-load $i --metrics-brief
     echo "$(date -u) Done CPU throttle for 5 minutes @ ${i}%"
+    sleep $(shuf -i 5-60 -n 1)
   done
 done
 
