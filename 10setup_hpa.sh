76dd701d4f9e0546ed64c9f3146e912731742354
Sajit Sasi <sajit.sasi@outlook.com>
2021-10-04 18:20:57 -0500

    made code changes

--- 10setup_hpa.sh
+++ 10setup_hpa.sh
@@ -7,7 +7,11 @@ DIR=$(pwd)
 runcmd "kubectl apply --namespace ratingsapp -f ${DIR}/yaml/${RATINGS_API_HPA_YAML}"
 
 #2. Run a load test with the HPA
-INGRESS_HOSTNAME=$(kubectl get services --namespace ingress | grep -v -e NAME -e kubectl -e "controller\-admission" | awk '{print $4}' | sed -e 's/\./\-/g')
+if [ ${ENABLE_APP_GATEWAY_INGRESS_CONTROLLER} == "true" ]; then
+    INGRESS_HOSTNAME=$(az network public-ip show -g ${AZ_RG} -n ${AZ_APP_GW_PUBLIC_IP} --query "ipAddress" -o tsv | sed 's/\./-/g')
+else
+    INGRESS_HOSTNAME=$(kubectl get services --namespace ingress | grep -v -e NAME -e kubectl -e "controller\-admission" | awk '{print $4}' | sed -e 's/\./\-/g')
+fi
 LOADTEST_API_ENDPOINT="https://frontend.${INGRESS_HOSTNAME}.nip.io/api/loadtest"
 printcmd "az container create \ -g ${AZ_RG} -n loadtest --cpu 4 --memory 1 --image azch/artillery --restart-policy Never --command-line \"artillery quick -r 500 -d 120 ${LOADTEST_API_ENDPOINT}\""
 az container create \
--- src/stress/stress.sh
+++ src/stress/stress.sh
@@ -5,7 +5,7 @@
 
 
 while [ 1 ]; do
-  for i in `seq 5 5 100`; do
+  for i in `seq 75 5 100`; do
     echo "$(date -u) Starting CPU throttle for 5 minutes @ ${i}%"
     stress-ng --cpu -1 --cpu-method all -t ${timeout} --cpu-load $i --metrics-brief
     echo "$(date -u) Done CPU throttle for 5 minutes @ ${i}%"
